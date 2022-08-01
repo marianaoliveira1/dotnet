95eb3a07fbcde5f22cefd1cd22fb2377db91fa5d
Mariana Oliveira Fernandes <mariana_oliveira@virgo.atlantico.net.br>
2022-08-01 17:50:21 -0300

    string interpolacao

---
+++ balta.io/SobreStrings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SobreStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //id => identificar um item
+            var id = Guid.NewGuid();
+            id.ToString();
+
+            id = new Guid();
+            
+            Console.WriteLine(id.ToString().Substring(0,8));
+        }
+    }
+}
