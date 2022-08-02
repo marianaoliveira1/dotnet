ec491f1eb55feead833a6f32737d0d41328066f6
Mariana Oliveira Fernandes <mariana_oliveira@virgo.atlantico.net.br>
2022-08-02 17:27:34 -0300

    numeros

---
+++ balta.io/Moedas/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using System;
+
+namespace Moedas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            decimal valor = 10.5658025m;
+
+            Console.WriteLine(Math.Round(valor));
+            Console.WriteLine(Math.Ceiling(valor));
+            Console.WriteLine(Math.Floor(valor));
+        }
+    }
+}
